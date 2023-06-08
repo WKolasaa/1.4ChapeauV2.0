47bd19a924a2140ba7d6fcfed864e7d6c8d84e4a
WKolasaa <wkolasaa@gmail.com>
2023-06-08 18:33:37 +0200

    Updated

--- ChapeauUI/TableOverview.cs
+++ ChapeauUI/TableOverview.cs
@@ -18,29 +18,26 @@ namespace ChapeauUI
     {
         Employee employee;
         TableService tableService;
-        OrderService orderService;
-        List<Table> tables = new List<Table>();
-        OrderItemService orderItemService;
-        List<OrderItem> orderItems = new List<OrderItem>();
-        Dictionary<Button, Table> buttonDictionary = new Dictionary<Button, Table>();
+        List<Table> tables;
+        Dictionary<Button, Table> buttonDictionary;
+
         public TableOverview(Employee employee)
         {
             this.employee = employee;
-           
 
+            tables = new List<Table>();
             tableService = new TableService();
-            orderService = new OrderService();
-            orderItemService = new OrderItemService();
             tables = tableService.GetAllTables();
+            buttonDictionary = new Dictionary<Button, Table>();
 
             InitializeComponent();
-            
+
             AssigneTableButtonHandler();
 
             this.CenterToScreen();
 
             UserNamelbl.Text = $"{employee.Name}";
-           
+
         }
 
         private void Logoutbtn_Click(object sender, EventArgs e)
@@ -63,7 +60,7 @@ namespace ChapeauUI
             tableOrderView.ShowDialog();
             this.Show();
             AssigneTableButtonHandler();
-          
+
 
         }
         private void TableButtonClick(object sender, EventArgs e)
@@ -83,38 +80,47 @@ namespace ChapeauUI
                  // create button, hint dictionary
                  // create eventhandler
              }*/
+            tablepanel.Controls.Clear(); // FUCKIGN LINE !!!!
             tables = tableService.GetAllTables();
-            Dictionary<Button, Table> tableButtonDictionary = new Dictionary<Button, Table>();
 
             for (int i = 0; i < tables.Count; i++)
             {
                 Button tableButton = new Button();
                 tableButton.Text = "Table " + (i + 1);
-                tableButton.Location = new Point(10, 10 + i * 30);
+                tableButton.Size = new Size(100, 50);
+                tableButton.Location = new Point(20 * (i % 3 * 10), 10 + (i / 3) * 50);
                 tableButton.Tag = i;
                 tableButton.Click += TableButtonClick;
-                tableButtonDictionary.Add(tableButton, tables[i]);
+                buttonDictionary.Add(GetColour(tables[i], tableButton), tables[i]);
 
                 // Add the button to a container
+
                 tablepanel.Controls.Add(tableButton);
-                switch (tables[i].TableStatus)
-                {
-                    case TableStatus.Occupied:
-                        tableButton.BackColor = Color.Yellow;
-                        break;
-                    case TableStatus.Free:
-                        tableButton.BackColor = Color.Green;
-                        break;
-                    case TableStatus.Reserved:
-                        tableButton.BackColor = Color.Red;
-                        break;
-                }
             }
+
         }
 
         private void Refreshbtn_Click(object sender, EventArgs e)
         {
             AssigneTableButtonHandler();
+        }
+
+        private Button GetColour(Table table, Button button)
+        {
+            switch (table.TableStatus)
+            {
+                case TableStatus.Occupied:
+                    button.BackColor = Color.Yellow;
+                    break;
+                case TableStatus.Free:
+                    button.BackColor = Color.Green;
+                    break;
+                case TableStatus.Reserved:
+                    button.BackColor = Color.Red;
+                    break;
+            }
+
+            return button;
         }
     }
 }
