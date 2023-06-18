c666edd3b9d7d0db077a2078ad7352ae8fff9aaa
WKolasaa <wkolasaa@gmail.com>
2023-06-18 14:44:14 +0200

    Repair

--- ChapeauService/OrderItemService.cs
+++ ChapeauService/OrderItemService.cs
@@ -82,19 +82,5 @@ namespace ChapeauService
               orderitemdao.UpdateMenu(menu);
           } */
 
-        public List<OrderItem> GetFoodStatusByTableId(int i)
-        {
-            return new List<OrderItem>();
-        }
-
-        public List<OrderItem> GetDrinkStatusByTableId(int i)
-        {
-            return new List<OrderItem>();
-        }
-
-        public bool CheckIfTableHasActiveOrders(Table table)
-        {
-            return true;
-        }
     }
 }
